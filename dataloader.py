9ed57577260af4a9d523037e116f8c013115cce7
Mathias Felix Gruber <mafg@novozymes.com>
2018-04-10 15:07:44 +0200

    Code cleanup

--- dataloader.py
+++ dataloader.py
@@ -6,7 +6,7 @@ from scipy.misc import imresize
 
 
 class DataLoader():
-    def __init__(self, datapath, height_hr, width_hr, height_lr, width_lr):
+    def __init__(self, datapath, height_hr, width_hr, height_lr, width_lr, scale):
 
         # Store the datapath
         self.datapath = datapath
@@ -14,47 +14,61 @@ class DataLoader():
         self.height_lr = height_lr
         self.width_hr = width_hr
         self.width_lr = width_lr
+        self.scale = scale
 
         # Get the paths for all the images
         self.img_paths = []
         for dirpath, _, filenames in os.walk(self.datapath):
-            for filename in [f for f in filenames if f.endswith(".JPEG")]:
+            for filename in [f for f in filenames if any(filetype in f.lower() for filetype in ['jpeg', 'png', 'jpg'])]:
                 self.img_paths.append(os.path.join(dirpath, filename))
+        print(f">> Found {len(self.img_paths)} images in dataset")
 
-    def load_batch(self, batch_size=1, img_paths=None):
+    def get_random_images(self, n_imgs=1):
+        """Get n_imgs random images from the dataset"""
+        return np.random.choice(self.img_paths, size=n_imgs)
+
+    def scale_imgs(self, imgs):
+        """Scale images prior to passing to SRGAN"""
+        return imgs / 127.5 - 1
+
+    def load_batch(self, batch_size=1, img_paths=None, training=True):
         """Loads a batch of images from datapath folder""" 
-        # TODO: In case of only one channel / images not being loaded, deal with that in a smarter way       
 
         # Pick a random set of images from the datapath if not already set
         if not img_paths:
-            img_paths = np.random.choice(self.img_paths, size=batch_size)
+            img_paths = self.get_random_images(batch_size)
 
         # Scale and pre-process images
         imgs_hr, imgs_lr = [], []
         for img_path in img_paths:            
 
-            # Load image and resize appropriately
+            # Load image
             img = imageio.imread(img_path).astype(np.float)            
-            img_hr = imresize(img, (self.height_hr, self.width_hr))
-            img_lr = imresize(img, (self.height_lr, self.width_lr))
 
-            print(f">> Reading image: {img_path}")
-            print(f">> Image shapes: {img.shape} {img_hr.shape}, {img_lr.shape}")
+            # If gray-scale, convert to RGB
+            if len(img.shape) == 2:
+                img = np.stack((img,)*3, -1)
 
-            # Assert 3 channels
-            if len(img.shape) == 3:
+            # Resize images appropriately
+            if training:
+                img_hr = imresize(img, (self.height_hr, self.width_hr))
+                img_lr = imresize(img, (self.height_lr, self.width_lr))
+            else:
+                lr_shape = (int(img.shape[0]/self.scale), int(img.shape[1]/self.scale))
+                img_hr = np.array(img)
+                img_lr = imresize(img, lr_shape)
 
-                # Keep
-                imgs_hr.append(img_hr)
-                imgs_lr.append(img_lr)
+            # print(f">> Reading image: {img_path}")
+            # print(f">> Image shapes: {img.shape} {img_hr.shape}, {img_lr.shape}")
+
+            # Store images
+            imgs_hr.append(self.scale_imgs(img_hr))
+            imgs_lr.append(self.scale_imgs(img_lr))
 
         # Scale images
-        imgs_hr = np.array(imgs_hr) / 127.5 - 1
-        imgs_lr = np.array(imgs_lr) / 127.5 - 1
-
-        # Check that we found pictures, otherwise search again
-        if len(imgs_hr) == 0:
-            imgs_hr, imgs_lr = self.load_batch(batch_size=batch_size)
+        if training:
+            imgs_hr = np.array(imgs_hr)
+            imgs_lr = np.array(imgs_lr)
 
         # Return image batch
         return imgs_hr, imgs_lr
